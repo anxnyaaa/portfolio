546120ed9036af5a4d4cac7a2b8612010bc2a1c3
anxnyaaa <ananya.pal@iemlabs.com>
2024-04-15 15:22:14 +0530

    pricing card done

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroTexts from '../components/HeroText'
+import PricingCard from '../components/PricingCard'
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
       <HeroTexts heading="PROJECTS" text="Some of my most recent works."/>
+      <PricingCard/>
       <Footer/>
     </div>
   )
