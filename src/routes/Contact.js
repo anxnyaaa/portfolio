6326e0d58fe5497deabb8d8e8bd7d85dd759cd9e
anxnyaaa <ananya.pal@iemlabs.com>
2024-04-16 13:21:38 +0530

    Contact page done

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroTexts from '../components/HeroText'
+import Form from '../components/Form'
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
       <HeroTexts heading="CONTACTS" text="Feel free to reach out."/>
+      <Form/>
       <Footer/>
     </div>
   )
